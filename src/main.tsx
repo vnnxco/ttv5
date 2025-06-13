1fe16ca861abd18c2823effb15f69b057b77e10b
thetailsxyz <thetailsxyz@gmail.com>
2025-06-13 22:15:46 +0800

    Apply dark theme, blue colors, and hide sidebar scrollbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Toaster } from 'sonner';
+import { ThemeProvider } from '@/components/theme-provider';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <Toaster />
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+      <App />
+      <Toaster />
+    </ThemeProvider>
   </StrictMode>
 );
