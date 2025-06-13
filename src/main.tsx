905f2a3cf4ba2858135874bba628df029d1b7183
thetailsxyz <thetailsxyz@gmail.com>
2025-06-14 00:00:01 +0800

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Toaster } from 'sonner';
-import { ThemeProvider } from '@/components/theme-provider';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-      <App />
-      <Toaster />
-    </ThemeProvider>
+    <App />
+    <Toaster />
   </StrictMode>
 );
