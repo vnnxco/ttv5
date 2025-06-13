c7d5d1627ead36eca8090411a12533867d950faa
thetailsxyz <thetailsxyz@gmail.com>
2025-06-13 21:58:09 +0800

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ export default {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-    './index.html'
   ],
   prefix: "",
   theme: {
@@ -61,13 +60,6 @@ export default {
           "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
           border: "hsl(var(--sidebar-border))",
           ring: "hsl(var(--sidebar-ring))",
-        },
-        chart: {
-          "1": "hsl(var(--chart-1))",
-          "2": "hsl(var(--chart-2))",
-          "3": "hsl(var(--chart-3))",
-          "4": "hsl(var(--chart-4))",
-          "5": "hsl(var(--chart-5))",
         },
       },
       borderRadius: {
